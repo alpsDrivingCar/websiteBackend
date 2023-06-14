c42ff32175feee10d5600846e1d7df57588e6918
MohammedMansour <mohammed.mansour4009@gmail.com>
2023-06-14 17:08:13 +0300

    add  home api

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ dotenv.config(dotenv);
 //Routes
 app.use('/api/booking/lesson', require('./routes/booking/lesson/LessonRoutes'))
 app.use('/api/booking/instructors', require('./routes/booking/instructors/InstructorsBookingRoutes'))
+app.use('/api/home', require('./routes/home/HomeRoutes'))
 
 
 
