17aeff5ab0b23fbbf111a2e05064c2495c2b56ff
MohammedMansour4009 <mohammed.mansour4009@gmail.com>
2023-09-02 12:44:53 +0300

    add payment way

--- app.js
+++ app.js
@@ -36,6 +36,51 @@ app.use(function (req, res, next) {
 });
 
 
+const stripe = require('stripe')(
+    process.env.STRIPE_PRIVATE_KEY
+)
+
+app.post('/create-checkout', async (req, res) => {
+    try {
+        const paymentIntent = await stripe.checkout.sessions.create({
+            // amount, // amount in cents
+            // currencycy: 'usd',
+            mode: 'payment', // Use 'payment' mode for one-time payments
+            success_url: req.body.success_url,
+            cancel_url: req.body.cancel_url,
+            line_items: req.body.items.map( item => {
+                return {
+                    price_data:{
+                        currency: 'usd',
+                        product_data:{
+                            name :item.name
+                        },
+                        unit_amount: item.price
+                    },
+                    quantity:item.quantity
+                }
+            }),
+        });
+
+        res.json({url: paymentIntent.url})
+    }catch (e) {
+        res.status(500).json({error : e.message})
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //mongoose
 const {mongoose} = require('mongoose');
 mongoose.connect(process.env.DATABASE, {
