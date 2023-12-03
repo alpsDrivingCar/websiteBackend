116a94265e27057d02bfde4920a50878095c4a32
mansour <alpsdatabeas@gmail.com>
2023-12-03 21:12:58 +0300

    add all

--- routes/gift/GiftRoutes.js
+++ routes/gift/GiftRoutes.js
@@ -2,7 +2,7 @@ const giftServer = require('../../Controller/gift/giftCotroller')
 const express = require("express");
 const router = express.Router();
 
-router.get("/",giftServer.gifts)
+router.get("/all",giftServer.gifts)
 router.post("/",giftServer.createGift)
 router.delete("/:id",giftServer.deleteGift)
 router.put("/",giftServer.giftUpdate)
