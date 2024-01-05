a5873b87a612f8f1220192aaa28b3dea359dca25
mansour <alpsdatabeas@gmail.com>
2024-01-06 00:28:39 +0300

    get test home

--- Controller/home/homeCotroller.js
+++ Controller/home/homeCotroller.js
@@ -15,14 +15,26 @@ exports.createHome = (req, res) => {
 }
 
 exports.homes = (req, res) => {
-    // result =   object  inside mongo database
-    // HomeSchema.findById(req.params.id)
+    const userAgent = req.headers['user-agent'];
+    let isMobile = /mobile/i.test(userAgent);
+    console.log("isMobile = " + isMobile)
+    console.log("userAgent = " + userAgent)
     HomeSchema.findById("6489c7239bdfd1bbc0d33afc")
         .then((result) => {
-            res.json(result)
+            // You can modify the result based on the device type if needed
+            if (isMobile) {
+                // Handle the response for mobile devices
+                // For example, modify 'result' or send a different response
+
+                result.title =  result.title + "..."
+            } else {
+                // Handle the response for non-mobile devices (like browsers)
+            }
+            res.json(result);
         })
         .catch((err) => {
             console.log(err);
+            res.status(500).send("Error retrieving data");
         });
 }
 
