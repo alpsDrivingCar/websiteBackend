3f47c1930412ef54c7d742da441e381ff59ed8b9
mansour <alpsdatabeas@gmail.com>
2024-01-18 00:08:17 +0300

    add base funcationc

--- Controller/booking/checkout/payment/updateOrder.js
+++ Controller/booking/checkout/payment/updateOrder.js
@@ -1,5 +1,6 @@
 const CheckoutInfo = require("../../../../model/booking/checkout/payment/paymentSchema");
 const mongoose = require("mongoose");
+const axios = require('axios');
 
 
 // In website after booking
@@ -15,31 +16,7 @@ exports.updateOrderStatus = async (req, res) => {
         const apiResponse = await fireExternalAPI(updatedCheckoutInfo);
         console.log("apiResponse.pupil.id = " + apiResponse.pupil._id)
 
-        const lessonEventData = {
-            "startTime": "12:30 AM",
-            "instructorId": updatedCheckoutInfo.orderInfo.instructorsId.toString(), // Convert ObjectId to string
-            "pupilId": apiResponse.pupil._id,
-            "durationMinutes": "60", // Duration of the lesson in minutes
-            "durationHours": "2", // Duration of the lesson in hours
-            "gearbox":"automatic", // Type of gearbox, e.g., automatic or manual
-            "lessonType": updatedCheckoutInfo.orderInfo.typeOfLesson, // Type of lesson, e.g., regular, intensive
-            "pickUpLocation": "home", // Pickup location
-            "dropOffLocation": "home", // Drop-off location
-            "date": "1/18/2024, 12:00:00 AM" // Date and time of the lesson
-        };
-
-        console.log('Start Time:', lessonEventData.startTime);
-        console.log('Instructor ID:', lessonEventData.instructorId);
-        console.log('Pupil ID:', lessonEventData.pupilId);
-        console.log('Duration Minutes:', lessonEventData.durationMinutes);
-        console.log('Duration Hours:', lessonEventData.durationHours);
-        console.log('Gearbox:', lessonEventData.gearbox);
-        console.log('Lesson Type:', lessonEventData.lessonType);
-        console.log('Pickup Location:', lessonEventData.pickUpLocation);
-        console.log('Drop-off Location:', lessonEventData.dropOffLocation);
-        console.log('Date:', lessonEventData.date);
-
-        const addLessonEvent1 = addLessonEvent(lessonEventData)
+        const addLessonEvent1 = await addLessonEvent()
             
         res.json({
             message: "Order status updated successfully!",
@@ -58,17 +35,28 @@ exports.updateOrderStatus = async (req, res) => {
     }
 };
 
-const axios = require('axios');
-
-async function addLessonEvent(eventData) {
+async function addLessonEvent() {
     try {
+        const lessonEventData = {
+            "startTime": "12:30 AM",
+            "instructorId": updatedCheckoutInfo.orderInfo.instructorsId.toString(), // Convert ObjectId to string
+            "pupilId": apiResponse.pupil._id,
+            "durationMinutes": "60", // Duration of the lesson in minutes
+            "durationHours": "2", // Duration of the lesson in hours
+            "gearbox":"automatic", // Type of gearbox, e.g., automatic or manual
+            "lessonType": updatedCheckoutInfo.orderInfo.typeOfLesson, // Type of lesson, e.g., regular, intensive
+            "pickUpLocation": "home", // Pickup location
+            "dropOffLocation": "home", // Drop-off location
+            "date": "1/18/2024, 12:00:00 AM" // Date and time of the lesson
+        };
+
         const apiUrl = 'https://alps-driving-car.herokuapp.com/api/diary/lesson-event';
         const headers = {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDEzNjZmYWNjM2FkNmRlNDU4ZDFjMzAiLCJmaXJzdE5hbWUiOiJtb2hhbW1lZDEiLCJsYXN0TmFtZSI6Im1hbnNvdXIxIiwiZW1haWwiOiJtb2hhbW1lZC5tYW5zb3VyNDAwOUBnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRibHh1TndQa05UYmxqL0wxUVM1ZnouQXZwU1EvUk93VEpyaC9qVmRJalI2dGp2b2xScVN5TyIsInBob25lTnVtYmVyIjoiMDc4ODEyMjYwOCIsImlzQWRtaW4iOnRydWUsImlzU3VwZXJ2aXNvciI6ZmFsc2UsImlzSW5zdHJ1Y3RvciI6ZmFsc2UsInJvbGVzIjpbInN1cGVydmlzb3IiXSwiY3JlYXRlZEF0IjoiMjAyMy0wMy0xNlQxODo1OTowNi41MDFaIiwidXBkYXRlZEF0IjoiMjAyNC0wMS0xNlQxNzowMzoxOS4yNzFaIiwiX192IjowLCJwZGZBZG1pbiI6W10sImRlc2NyaXB0aW9uIjoiQmFheiBpcyB0aGUgZmlyc3QgQXJhYmljIHNvY2lhbCBtZWRpYSBwbGF0Zm9ybSB0aGF0IHByb3ZpZGVzIGNvbW11bml0aWVzIGZvciB1c2VycyB0byBjb25uZWN0IHdpdGggb3RoZXJzIHdobyBzaGFyZSBzaW1pbGFyIGludGVyZXN0cywgaG9iYmllcyBhbmQgcGFzc2lvbnMuZWVlZmRcbiIsInByb2ZpbGVJbWFnZSI6Imh0dHBzOi8vZmlyZWJhc2VzdG9yYWdlLmdvb2dsZWFwaXMuY29tL3YwL2IvYWxwcy0xZjYwNi5hcHBzcG90LmNvbS9vL3N1cGVydmlzb3IlMkYxNzA0NTYyODI1MTk1Y3JvcHBlZC1pbWFnZS5qcGc_YWx0PW1lZGlhJnRva2VuPTBhYjg3ZTc1LWQxOTMtNGQyOS04NDI1LWU1NTZmYjc2MjE0YiIsImlhdCI6MTcwNTUxMTcyMywiZXhwIjoxNzA1NTk4MTIzfQ.Pi9CxSzD2YI4Y9tgTeVd482B-RM_xRtO5nOOIxUqVLs'
         };
 
-        const response = await axios.post(apiUrl, eventData, { headers });
+        const response = await axios.post(apiUrl, lessonEventData, { headers });
 
         if (response.status < 200 || response.status >= 300) {
             throw new Error(`API responded with status code ${response.status}`);
@@ -84,7 +72,7 @@ async function addLessonEvent(eventData) {
         } else if (error.request) {
             throw new Error('API did not respond');
         } else {
-            throw new Error('Error in setting up the API request');
+            throw new Error('Add LessonEvent Error in setting up the API request =' + error.message );
         }
     }
 }
@@ -132,7 +120,6 @@ async function fireExternalAPI(updatedCheckoutInfo) {
 }
 
 async function updateStatusById(id, status) {
-    // Validation
     if (!mongoose.isValidObjectId(id)) {
         throw new Error('Invalid ID format');
     }
@@ -142,7 +129,6 @@ async function updateStatusById(id, status) {
         throw new Error('Invalid status value');
     }
 
-    // Update operation
     const updatedCheckoutInfo = await CheckoutInfo.findByIdAndUpdate(
         id,
         { 'orderInfo.status': status },
