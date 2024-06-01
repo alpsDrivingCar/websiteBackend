3d44a9f23f5d764f0b0f44a69e0de2c701aa93e7
MohammedMansour4009 <mohammed.mansour4009@gmail.com>
2024-06-01 14:27:12 +0300

    fix test booking

--- model/setting/testBooking/testBookingInfoSchema.js
+++ model/setting/testBooking/testBookingInfoSchema.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const contactSectionSchema = new Schema({
+  content:String,
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  whatsNumber: {
+    type: String,
+    required: true,
+  }
+}, { _id : false }); // Disabling _id for subdocuments
+
+
 const testBookingSchema = new Schema({
   title: {
     type: String,
@@ -15,8 +28,8 @@ const testBookingSchema = new Schema({
     required: false, // Assuming the image URL is optional
   },
   contactSectionData: {
-    phoneNumber:  String,
-    whatsNumber: String
+    type: contactSectionSchema,
+    required: true,
   },
 }, {
   timestamps: true
