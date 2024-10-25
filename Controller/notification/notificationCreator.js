cdc79a2d6785bb51f04250dd73df49c90984b80f
shamstash <100798368+shamstash@users.noreply.github.com>
2024-10-25 17:50:59 +0300

    Send-joinus-notification-LPS-18

--- Controller/notification/notificationCreator.js
+++ Controller/notification/notificationCreator.js
@@ -1,29 +1,42 @@
 const Notification = require('../../model/Notification/Notification'); // Adjust the path as necessary
+const { mongoose } = require('mongoose');
 
 ///////////////////////////////////////////////
 //////////////     admins        //////////////
 ///////////////////////////////////////////////
 const createWebsiteAdminNotification = async (name, websiteType, pageId, pageSlug) => {
     try {
-        let notificationData = {
-            notificationTemplate: "665c6ef5d91dd3e0ac11cb0f",
-            replacement: {
-                websiteType: websiteType,
-                name: name
-            },
-            user: {
-                id: "65abc62447f75c99188c2cc0",
-                userType: "AllAdmin"
-            },
-            pageDirected: {
-                id: pageId,
-                slug: pageSlug
-            }
-        };
 
-        const newNotification = new Notification(notificationData);
-        console.log("crewaet ");
-        return await newNotification.save();
+        const adminIds = await mongoose.connection.db
+            .collection('admins')
+            .find({})
+            .project({ _id: 1 })
+            .toArray();
+
+
+        const adminIdStrings = adminIds.map(doc => doc._id.toString());
+        const notificationTemplate = websiteType === "Joinus" ? "671b8ba097c9f58eb0f61373" : "665c6ef5d91dd3e0ac11cb0f";
+
+        const notifications = adminIdStrings.map(adminId => {
+            return {
+                notificationTemplate: notificationTemplate,
+                replacement: {
+                    websiteType: websiteType,
+                    name: name
+                },
+                user: {
+                    id: adminId,
+                    userType: "AllAdmin"
+                },
+                pageDirected: {
+                    id: pageId,
+                    slug: pageSlug
+                }
+            };
+        });
+
+        // Insert notifications into the database
+        await Notification.insertMany(notifications);
 
     } catch (err) {
         console.log(err);
