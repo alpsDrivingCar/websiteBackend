e2de6a0f84b95af4c2363340a0615496b175314f
shamstash <100798368+shamstash@users.noreply.github.com>
2024-11-17 16:55:44 +0300

    LPS-66 dont save invalid postcodes to database

--- Controller/booking/postcode/postcodeCotroller.js
+++ Controller/booking/postcode/postcodeCotroller.js
@@ -3,60 +3,65 @@ const PostcodeSearch = require("../../../model/postcode/PostcodeSearchSchema");
 const PackageSchema = require("../../../model/booking/package/packageSchema");
 
 exports.validateUKPostcode = async (req, res) => {
-    let postcode = req.query.postcode;
-    postcode = postcode.replace(/\s+/g, '').toUpperCase();
+    try {
+        let postcode = req.query.postcode;
+        postcode = postcode.replace(/\s+/g, '').toUpperCase();
 
-    // Check for booking packages first
-    const hasPackages = await fetchBookingPackages(postcode);
-    if (!hasPackages) {
-        return res.status(404).json({ message: 'No trainers found. Please try another PostCode, such as NN2 8FW.' });
+        // Check for booking packages first
+        const hasPackages = await fetchBookingPackages(postcode);
+        if (!hasPackages) {
+            await trackPostcodeSearch(postcode, false);
+            return res.status(404).json({ message: 'No trainers found. Please try another PostCode, such as NN2 8FW.' });
+        }
+
+        const regexPattern = /^([Gg][Ii][Rr] ?0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) ?[0-9][A-Za-z]{2})$/;
+        if (!regexPattern.test(postcode)) {
+            return res.status(400).json({ valid: false, message: 'Invalid UK postcode.' });
+        }
+
+        const areaLength = determinePostcodeAreaLength(postcode);
+        const areaRegex = new RegExp("^" + postcode.substring(0, areaLength), "i");
+
+        const filter = {
+            "areas": { $regex: areaRegex },
+            "AcceptStudent": true,
+            "status": "active"
+        };
+
+        const result = await InstructorsUserSchema.find(filter);
+        await trackPostcodeSearch(postcode, result.length > 0);
+
+        if (result.length === 0) {
+            return res.status(404).json({ message: 'No trainers found. Please try another PostCode, such as NN2 8FW' });
+        } else {
+            return res.status(204).send();
+        }
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'An error occurred while processing your request.' });
     }
-
-    const regexPattern = /^([Gg][Ii][Rr] ?0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) ?[0-9][A-Za-z]{2})$/;
-    if (!regexPattern.test(postcode)) {
-        return res.status(400).json({ valid: false, message: 'Invalid UK postcode.' });
-    }
-
-    const areaLength = determinePostcodeAreaLength(postcode);
-    const areaRegex = new RegExp("^" + postcode.substring(0, areaLength), "i");
-
-    const filter = {
-        "areas": { $regex: areaRegex },
-        "AcceptStudent": true,
-        "status": "active"
-    };
-
-    InstructorsUserSchema.find(filter)
-        .then(async result => {
-            const searchType = result.length === 0 ? 'postcodeNotExisting' : 'postcodeExisting';
-            const documentId = "65a173b412374a1d2241edf0";
-
-            // First, try to increment the searchCount if the postcode exists
-            const updateResult = await PostcodeSearch.updateOne(
-                { _id: documentId, [`${searchType}.name`]: postcode },
-                { $inc: { [`${searchType}.$[elem].searchCount`]: 1 } },
-                { arrayFilters: [{ "elem.name": postcode }] }
-            );
-
-            // If the postcode was not in the array and no document was updated, add it
-            if (updateResult.matchedCount === 0 || updateResult.modifiedCount === 0) {
-                await PostcodeSearch.updateOne(
-                    { _id: documentId },
-                    { $push: { [searchType]: { name: postcode, searchCount: 1 } } }
-                );
-            }
-
-            if (result.length === 0) {
-                return res.status(404).json({ message: 'No trainers found. Please try another PostCode, such as NN2 8FW' });
-            } else {
-                return res.status(204).send();
-            }
-        })
-        .catch(err => {
-            console.error(err);
-            return res.status(500).json({ message: 'An error occurred while processing your request.' });
-        });
 };
+
+// Helper function to track postcode searches
+async function trackPostcodeSearch(postcode, exists) {
+    const documentId = "65a173b412374a1d2241edf0";
+    const searchType = exists ? 'postcodeExisting' : 'postcodeNotExisting';
+    console.log("Got Here111")
+    // Try to increment the searchCount if the postcode exists
+    const updateResult = await PostcodeSearch.updateOne(
+        { _id: documentId, [`${searchType}.name`]: postcode },
+        { $inc: { [`${searchType}.$[elem].searchCount`]: 1 } },
+        { arrayFilters: [{ "elem.name": postcode }] }
+    );
+
+    // If the postcode was not in the array and no document was updated, add it
+    if (updateResult.matchedCount === 0 || updateResult.modifiedCount === 0) {
+        await PostcodeSearch.updateOne(
+            { _id: documentId },
+            { $push: { [searchType]: { name: postcode, searchCount: 1 } } }
+        );
+    }
+}
 
 const determinePostcodeAreaLength = (postcode) => {
     switch (postcode.length) {
