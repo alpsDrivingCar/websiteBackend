22268f22ec42dc90152d02d9e17639c564acdc7e
shamstash <100798368+shamstash@users.noreply.github.com>
2024-11-25 18:28:38 +0300

    send one notificaton for all the admins

--- Controller/notification/notificationCreator.js
+++ Controller/notification/notificationCreator.js
@@ -6,41 +6,31 @@ const { mongoose } = require('mongoose');
 ///////////////////////////////////////////////
 const createWebsiteAdminNotification = async (name, websiteType, pageId, pageSlug) => {
     try {
-
-        const adminIds = await mongoose.connection.db
-            .collection('admins')
-            .find({})
-            .project({ _id: 1 })
-            .toArray();
-
-
-        const adminIdStrings = adminIds.map(doc => doc._id.toString());
+        const commonAdminId = "6743505365e60677769330af";
         const notificationTemplate = websiteType === "Joinus" ? "671b8ba097c9f58eb0f61373" : "665c6ef5d91dd3e0ac11cb0f";
 
-        const notifications = adminIdStrings.map(adminId => {
-            return {
-                notificationTemplate: notificationTemplate,
-                replacement: {
-                    websiteType: websiteType,
-                    name: name
-                },
-                user: {
-                    id: adminId,
-                    userType: "AllAdmin"
-                },
-                pageDirected: {
-                    id: pageId,
-                    slug: pageSlug
-                }
-            };
-        });
+        const notification = {
+            notificationTemplate: notificationTemplate,
+            replacement: {
+                websiteType: websiteType,
+                name: name
+            },
+            user: {
+                id: commonAdminId,
+                userType: "AllAdmin"
+            },
+            pageDirected: {
+                id: pageId,
+                slug: pageSlug
+            }
+        };
 
-        // Insert notifications into the database
-        await Notification.insertMany(notifications);
+        // Insert single notification into the database
+        await Notification.create(notification);
 
     } catch (err) {
         console.log(err);
-        throw err; // Rethrow the error to be handled by the caller
+        throw err;
     }
 }
 
