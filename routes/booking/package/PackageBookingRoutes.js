f5202c1e41cbcea6a68e2da8ac4ec39b97640f3c
Ahmad Tash <douqa21@gmail.com>
2026-01-09 19:18:39 +0300

    mock test - fix auth

--- routes/booking/package/PackageBookingRoutes.js
+++ routes/booking/package/PackageBookingRoutes.js
@@ -1,16 +1,17 @@
 const server = require('../../../Controller/booking/package/PackageBookingCotroller')
+const authenticateAdmin = require('../../../Middleware/dashboardAdminAuth')
 const express = require("express");
 const router = express.Router();
 
-router.get("/by-id/:id", server.getBookingPackageById)
-router.get("/by-post-code", server.bookingPackageByPostCode)
-router.get("/by-post-code-and-type", server.bookingPackageByPostCodeAndType)
+router.get("/by-id/:id", authenticateAdmin,server.getBookingPackageById)
+router.get("/by-post-code", authenticateAdmin,server.bookingPackageByPostCode)
+router.get("/by-post-code-and-type", authenticateAdmin,server.bookingPackageByPostCodeAndType)
 router.get("/mock-test-offer", server.getMockTestBookingPackage)
-router.post("/", server.createBookingPackage)
-router.put("/:id", server.updateBookingPackage)
-router.put("/:id/status", server.updateBookingPackageStatus)  
-router.delete("/:id", server.deleteBookingPackage)
-router.post("/slug-type", server.getPackagesBySlug)
+router.post("/", authenticateAdmin,server.createBookingPackage)
+router.put("/:id", authenticateAdmin,server.updateBookingPackage)
+router.put("/:id/status", authenticateAdmin,server.updateBookingPackageStatus)  
+router.delete("/:id", authenticateAdmin,server.deleteBookingPackage)
+router.post("/slug-type", authenticateAdmin,server.getPackagesBySlug)
 
 
 module.exports = router
